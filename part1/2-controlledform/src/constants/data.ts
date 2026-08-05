74cc79cfbda8c55b79b4f272745010da9941f135
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-08-05 14:51:47 +0330

    add constanst data

---
+++ part1/2-controlledform/src/constants/data.ts
@@ -0,0 +1,24 @@
+
+type inputsDataType = {
+    id: number;
+    label: string;
+    type: string;
+
+
+};
+
+
+export const inputsData: inputsDataType[] = [
+
+
+    { id: 1, label: "userName", type: "text" },
+    { id: 2, label: "email", type: "email" },
+    { id: 3, label: "phone", type: "text" },
+    { id: 4, label: "select", type: "select" },
+    { id: 5, label: "female", type: "radio" },
+    { id: 6, label: "male", type: "radio" },
+    { id: 7, label: "please accpet terms and condition", type: "checkBox" },
+
+
+
+]
