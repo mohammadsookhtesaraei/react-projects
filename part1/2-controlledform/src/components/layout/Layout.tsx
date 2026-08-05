6405e8ad732e5bd1249c9a49619cd75b9c7e1b50
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-08-05 14:10:39 +0330

    add layout

---
+++ part1/2-controlledform/src/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react"
+
+type LayoutProps={
+    children:ReactNode
+};
+
+
+const Layout = ({children}:LayoutProps) => {
+  return (
+    <>
+    {children}
+    </>
+  )
+}
+export default Layout
