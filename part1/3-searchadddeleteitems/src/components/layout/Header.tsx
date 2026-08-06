c47c63b977bae4ea0a9696e9d740227481b96141
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-08-06 14:49:19 +0330

    add header

--- part1/3-searchadddeleteitems/src/components/layout/Header.tsx
+++ part1/3-searchadddeleteitems/src/components/layout/Header.tsx
@@ -1,6 +1,10 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="border-b border-b-gray-400/30 shadow-md bg-white">
+        <div className="h-10 flex items-center container">
+            <p className="text-gray-600">welcome</p>
+        </div>
+    </header>
   )
 }
 export default Header
