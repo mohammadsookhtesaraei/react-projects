b8dc925bec38f862463c0ac82332170f5f8cb8c2
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-08-06 14:51:52 +0330

    add homepage

--- part1/3-searchadddeleteitems/src/App.tsx
+++ part1/3-searchadddeleteitems/src/App.tsx
@@ -1,8 +1,11 @@
 import Layout from "./components/layout/Layout";
+import HomePage from "./pages/home/HomePage";
 
 const App = () => {
   return (
-    <Layout>App</Layout>
+    <Layout>
+      <HomePage/>
+    </Layout>
   )
 }
 export default App
