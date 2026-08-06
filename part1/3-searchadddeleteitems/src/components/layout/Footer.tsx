3bcf1502fb224c22bbae8c2abf14aef0d691b6a8
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-08-06 14:50:37 +0330

    add footer

--- part1/3-searchadddeleteitems/src/components/layout/Footer.tsx
+++ part1/3-searchadddeleteitems/src/components/layout/Footer.tsx
@@ -1,6 +1,10 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-blue-400">
+        <div className="h-10 flex items-center justify-center container">
+            <p className="text-white">developedby me</p>
+        </div>
+    </footer>
   )
 }
 export default Footer
--- part1/3-searchadddeleteitems/src/components/layout/Header.tsx
+++ part1/3-searchadddeleteitems/src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="border-b border-b-gray-400/30 shadow-md bg-white">
+    <header className="border-b border-b-gray-400/30 shadow-md bg-white mb-8">
         <div className="h-10 flex items-center container">
             <p className="text-gray-600">welcome</p>
         </div>
