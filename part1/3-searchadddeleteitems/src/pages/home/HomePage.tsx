a8d5e9fb313f93aac8a3ccd4f71238d9482df223
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-08-07 14:12:14 +0330

    add comment

--- part1/3-searchadddeleteitems/src/pages/home/HomePage.tsx
+++ part1/3-searchadddeleteitems/src/pages/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react"
+import { useState } from "react"
 import AddItem from "../../components/additem/AddItem"
 import ListItems from "../../components/listItems/ListItems"
 import { useLocalStorage } from "../../hooks/useLocalStorage";
@@ -10,18 +10,23 @@ export type DisplayType={
 };
 
 const HomePage = () => {
-  
+  // lift state-user state 
+
+  // state for get information from client and send it as props to additem component
   const [user,setUser]=useState("");
+
+  // show information list and send to as props to listItems component
   const [display,setDisplay]=useLocalStorage<DisplayType[]>("display",[]);
  
 
 
-
+// callback fn for  onchange input in additem component send as props
   const handleUserChangeInput=(event:React.ChangeEvent<HTMLInputElement>)=>{
   const {value}=event.target;
    setUser(value);
   };
 
+  // add item  in display state array for listrendering and send it as props to add item component
   const handleAddButtonClick=()=>{
         if(!user || !user.trim()){
         return
@@ -34,27 +39,24 @@ const HomePage = () => {
     setUser("");
   };
 
-
+// remove callback fn for table send as props to listItems then send to table component
   const handlRemoveItemButtonClick=(id:number)=>{
     const newItem=display.filter((item)=> item.id !== id);
     setDisplay(newItem);
   };
    
+  // edit callback fn for table send as props to listItems then send to table component
     const handlEditItemButtonClick=(id:number,edit:string)=>{
      const items=[...display];
 
-     const newItem=items.map((item)=>{
+     const newList=items.map((item)=>{
       if(item.id === id){
         return {...item,username:edit}
       };
       return item
 
      });
-
-
-     setDisplay(newItem);
-
-     
+     setDisplay(newList);
 
   };
 
