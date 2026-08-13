041ff392ffb63de8649064b341caf4e6ed0b9d63
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-08-13 13:59:47 +0330

    add fav component

--- part1/4-library/src/components/FavoriteBooks/FavoriteBooks.tsx
+++ part1/4-library/src/components/FavoriteBooks/FavoriteBooks.tsx
@@ -1,6 +1,25 @@
-const FavoriteBooks = () => {
+import type { ReactNode } from "react"
+import type { BookType } from "../../constants/mockData"
+
+import styles from "./FavoriteBooks.module.css"
+
+
+type Props={
+liked:BookType[]
+
+}
+
+const FavoriteBooks = ({liked}:Props):ReactNode => {
   return (
-    <div>FavoriteBooks</div>
+    <div className={styles.container}>
+      <h2>fav list</h2>
+      {liked.map((item)=>(
+        <div className={styles.card} key={item.id}>
+          <h2>{item.title}</h2>
+          <img src={item.image} alt="" />
+        </div>
+      ))}
+    </div>
   )
 }
 export default FavoriteBooks
