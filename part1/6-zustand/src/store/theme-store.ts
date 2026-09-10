2b46fc19a1565041e117ba2e5028187e9e4bf251
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-10 19:36:20 +0330

    add comment to theme store

--- part1/6-zustand/src/store/theme-store.ts
+++ part1/6-zustand/src/store/theme-store.ts
@@ -1,28 +1,37 @@
 import { create } from "zustand";
 
-type Theme="light" | "dark";
+type Theme = "light" | "dark";
 
-type ThemeStore={
-    theme:Theme,
-    onToggle:()=>void
+type ThemeStore = {
+    theme: Theme,
+    onToggle: () => void
 };
 
 
 
-export const useTheme=create<ThemeStore>((set,get)=>({
-    theme:"light",
-    onToggle:()=>{
-        const cuurentTheme=get().theme;
-        if(cuurentTheme === "light") {
+export const useTheme = create<ThemeStore>((set, get) => ({
+    theme: "light",
+    onToggle: () => {
+
+        // first way
+        //   set((state) => ({
+        //     theme: state.theme === "light" ? "dark" : "light",
+        //   }));
+
+        // second way how to use get
+        // گت میاد مقدار استیت در حال حاضر رو میده
+        // حواست باشه کلون نمیکنه
+        const cuurentTheme = get().theme;
+        if (cuurentTheme === "light") {
             set({
-                theme:"dark"
+                theme: "dark"
             })
-        }else {
+        } else {
 
             set({
-                theme:"light"
+                theme: "light"
             })
         }
-        
+
     }
 }))
