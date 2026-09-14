78eb062eb20776abaf4805629035206a0afb4b29
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-14 18:29:09 +0330

    add router

--- part1/7-auth/src/router/MyRoutes.tsx
+++ part1/7-auth/src/router/MyRoutes.tsx
@@ -1,10 +1,24 @@
-import type { ReactNode } from "react"
+import type { ReactNode } from "react";
 
-import { Routes,Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
+import RootLayout from "../Layouts/RootLayout/RootLayout";
+import HomePage from "../pages/HomePage/HomePage";
+import AdminPage from "../pages/AdminPage/AdminPage";
+import AuthPage from "../pages/AuthPage/AuthPage";
+import DashboardPage from "../pages/DashboardPage/DashboardPage";
+import Notfound from "../pages/Notfound/Notfound";
 
-const MyRoutes = ():ReactNode => {
+const MyRoutes = (): ReactNode => {
   return (
-    <>MyRoutes</>
-  )
-}
-export default MyRoutes
+    <Routes>
+      <Route element={<RootLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="admin" element={<AdminPage />} />
+        <Route path="auth" element={<AuthPage />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="*" element={<Notfound />} />
+      </Route>
+    </Routes>
+  );
+};
+export default MyRoutes;
