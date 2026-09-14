6612b06bee6637e1af9947d7c032583adad12ca3
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-14 18:19:33 +0330

    add axios and set http services

---
+++ part1/7-auth/src/services/http.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+
+const app = axios.create({
+    baseURL: "http://localhost:3400/",
+    headers: {
+        "Content-Type": "application/json"
+    }
+});
+
+
+
+app.interceptors.request.use(
+    (request) => {
+        return request
+    },
+    (error) => Promise.reject(error)
+);
+
+
+
+app.interceptors.response.use(
+    (response) => response.data,
+    (error) => Promise.reject(error)
+);
+
+
+const http = {
+    get: app.get,
+    post: app.post,
+    delete: app.delete,
+    patch: app.patch,
+    put: app.put,
+};
+
+
+export default http;
