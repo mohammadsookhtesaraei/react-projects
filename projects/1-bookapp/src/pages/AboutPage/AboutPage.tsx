ea2fd0fb157d4bedd7c50b4ebf5f9de4398e56b8
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-19 11:30:41 +0330

    add about page

--- projects/1-bookapp/src/pages/AboutPage/AboutPage.tsx
+++ projects/1-bookapp/src/pages/AboutPage/AboutPage.tsx
@@ -1,13 +1,57 @@
-import type { ReactNode } from "react"
 
-import Header from "../../components/Header/Header";
+import aboutImg from "../../assets/images/about-avatar.jpg";
 
- const AboutPage = ():ReactNode => {
-  return (
-    <div className="">
-      <Header/>
-    </div>
-  );
+export default function AboutPage() {
+    return (
+        <div>
+          {/* header */}
+            <section className="bg-linear-to-tr from-[#fd6969] to-[#d82828] text-white text-center
+       p-12 rounded-b-[50px]">
+                <h1 className="text-3xl md:text-4xl  mb-8"> درباره ما</h1>
+                <p className="text-lg opacity-90">
+                    با فرنود بوک، دنیای کتاب‌های صوتی و الکترونیکی همیشه در دسترس شماست.
+                </p>
+            </section>
+            
+            {/* content */}
+            <section className='flex flex-wrap  justify-center items-center gap-8 py-16 px-6 max-w-300 mx-auto ' >
+
+                {/*right section - text*/}
+                <div className='flex-1 min-w-75' >
+                    <h3 className='text-red-600 font-bold text-[28px] mb-5 text-justify' >فرنود بوک چیست؟</h3>
+                    <p className='text-[16px]  text-gray-600 py-1 leading-10 ' >فرنود بوک یک فروشگاه اینترنتی کتاب است که تلاش می‌کند تجربه‌ای متفاوت از مطالعه را به شما هدیه دهد. ما مجموعه‌ای بزرگ از کتاب‌های صوتی و الکترونیکی را در اختیار شما قرار داده‌ایم تا بتوانید در هر زمان و مکان به دنیای کتاب وارد شوید.
+
+                        در فرنود بوک، شما می‌توانید کتاب‌ها را به صورت آنلاین خریداری کنید، امانت بگیرید و حتی از بخش کتاب‌های رایگان لذت ببرید. هدف ما این است که مطالعه را آسان‌تر و لذت‌بخش‌تر کنیم.</p>
+                </div>
+                
+                {/*left section - image*/}
+                <div className=' flex-1 min-w-75 flex justify-center' >
+                    <img
+                        className='w-full max-w-112.5 shadow-2xl rounded-2xl '
+                        src={aboutImg} />
+                </div>
+            </section>
+
+            {/*values of farond book section */}
+            <section className=' mx-auto bg-[#fbcccc] p-7  text-center' >
+                <h4 className='text-center text-red-600 text-2xl font-semibold mb-10 ' >ارزش های فرنود بوک</h4>
+                     {/*boxs*/}
+                <div className='flex flex-wrap justify-center items-center max-w-250 mx-auto  gap-2' >
+                    <div className='bg-white shadow-xl flex-1 py-5 px-2 min-w-55 rounded-lg ' >
+                        <p className='text-red-600 text-lg my-4' >📚 دسترسی آسان</p>
+                        <span className='text-gray-700' >کتاب‌های صوتی و الکترونیکی همیشه همراه شما هستند، چه در موبایل، چه در تبلت و چه در لپ‌تاپ.</span>
+                    </div>
+                     <div className='bg-white shadow-xl flex-1 py-5 px-2 min-w-55 rounded-lg ' >
+                        <p className='text-red-600 text-lg my-4' >📚 دسترسی آسان</p>
+                        <span className='text-gray-700' >کتاب‌های صوتی و الکترونیکی همیشه همراه شما هستند، چه در موبایل، چه در تبلت و چه در لپ‌تاپ.</span>
+                    </div>
+                    <div className='bg-white shadow-xl flex-1 py-5 px-2 min-w-55 rounded-lg ' >
+                        <p className='text-red-600 text-lg my-4' >📚 دسترسی آسان</p>
+                        <span className='text-gray-700' >کتاب‌های صوتی و الکترونیکی همیشه همراه شما هستند، چه در موبایل، چه در تبلت و چه در لپ‌تاپ.</span>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    )
 }
-
-export default AboutPage;
--- projects/1-bookapp/src/pages/Contact/Contact.tsx
+++ projects/1-bookapp/src/pages/Contact/Contact.tsx
@@ -1,29 +1,32 @@
+import type { ReactNode } from "react";
 
 import Header from "../../components/Header/Header";
 
-import ContactImg from "../../assets/images/contact.jpg"
-import type { ReactNode } from "react";
+import ContactImg from "../../assets/images/contact.jpg";
 import { HiOutlineMail } from "react-icons/hi";
 import { FaPhone } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 
- const Contact = ():ReactNode => {
-
-
-
+const Contact = (): ReactNode => {
   return (
     <div>
-      <Header/>
-    {/* contact-header */}
-      <section className="bg-linear-to-tr from-[#fd6969] to-[#d82828] text-white text-center p-20 rounded-b-[50px]" >
-       <h1 className="text-3xl md:text-4xl mb-4">تماس با ما</h1>
-       <p className="text-lg opacity-90">هر سوالی دارید خوشحال می شیم باما در ارتباط باشید</p>
+      <Header />
+      {/* contact-header */}
+      <section className="bg-linear-to-tr from-[#fd6969] to-[#d82828] text-white text-center p-20 rounded-b-[50px]">
+        <h1 className="text-3xl md:text-4xl mb-4">تماس با ما</h1>
+        <p className="text-lg opacity-90">
+          هر سوالی دارید خوشحال می شیم باما در ارتباط باشید
+        </p>
       </section>
-     {/* contact-content */}
+      {/* contact-content */}
       <section className="flex flex-col md:flex-row gap-10 max-w-6xl mx-auto py-16 px-4">
         {/* right */}
         <div className="flex-1 min-w-75 max-w-md rounded-xl">
-          <img className="w-full object-cover h-75 rounded-2xl shadow-md" src={ContactImg} alt="تماس با ماما با فرنود" />
+          <img
+            className="w-full object-cover h-75 rounded-2xl shadow-md"
+            src={ContactImg}
+            alt="تماس با ماما با فرنود"
+          />
         </div>
         {/* left */}
         <div className="flex-1 flex flex-col gap-8">
@@ -31,41 +34,70 @@ import { FaLocationDot } from "react-icons/fa6";
           <div className="bg-white p-8 rounded-2xl shadow-md">
             <h2 className="text-2xl font-bold text-rose-600 mb-6">تماس باما</h2>
             <form className="flex flex-col">
-              <label className="font-semibold mb-2 text-[#333]" htmlFor="userName">نام</label>
-              <input type="text" id="userName" className="border border-gray-300 rounded-2xl px-4 py-3 mb-4" placeholder="نام" />
+              <label
+                className="font-semibold mb-2 text-[#333]"
+                htmlFor="userName"
+              >
+                نام
+              </label>
+              <input
+                type="text"
+                id="userName"
+                className="border border-gray-300 rounded-2xl px-4 py-3 mb-4"
+                placeholder="نام"
+              />
 
-              <label className="font-semibold mb-2 text-[#333]" htmlFor="email">ایمیل</label>
-              <input type="text" id="email" className="border border-gray-300 rounded-2xl px-4 py-3 mb-4" placeholder="example@gmail.com" />
-              
-              <label className="font-semibold mb-2 text-[#333]" htmlFor="text">پیام</label>
-              <textarea className="border border-gray-300 rounded-2xl px-4 py-3 " rows={8} id="text" placeholder="پیام خود را ارسال کنید"></textarea>
-              <button type="submit" className="p-2 self-start my-2 rounded-sm mx-2 shadow-sm cursor-pointer hover:scale-105 transition-transform duration-200 text-lg text-white bg-linear-to-r from-[#bd1e1e] to-[#940000]">
-             ارسال پیام
-            </button>
+              <label className="font-semibold mb-2 text-[#333]" htmlFor="email">
+                ایمیل
+              </label>
+              <input
+                type="text"
+                id="email"
+                className="border border-gray-300 rounded-2xl px-4 py-3 mb-4"
+                placeholder="example@gmail.com"
+              />
+
+              <label className="font-semibold mb-2 text-[#333]" htmlFor="text">
+                پیام
+              </label>
+              <textarea
+                className="border border-gray-300 rounded-2xl px-4 py-3 "
+                rows={8}
+                id="text"
+                placeholder="پیام خود را ارسال کنید"
+              ></textarea>
+              <button
+                type="submit"
+                className="p-2 self-start my-2 rounded-sm mx-2 shadow-sm cursor-pointer hover:scale-105 transition-transform duration-200 text-lg text-white bg-linear-to-r from-[#bd1e1e] to-[#940000]"
+              >
+                ارسال پیام
+              </button>
             </form>
           </div>
           {/* box */}
           <div className="bg-white p-8 rounded-2xl shadow-md">
-              <h2 className="text-2xl font-bold text-rose-600 mb-6">اطلاعات تماس</h2>
-              <ul className="space-y-4">
-               <li className="flex items-center gap-3">
-                <HiOutlineMail className="text-red-600 text-xl"/>
+            <h2 className="text-2xl font-bold text-rose-600 mb-6">
+              اطلاعات تماس
+            </h2>
+            <ul className="space-y-4">
+              <li className="flex items-center gap-3">
+                <HiOutlineMail className="text-red-600 text-xl" />
                 ایمیل:example@gmail.com
-               </li>
-               <li className="flex items-center gap-3">
-                <FaPhone className="text-red-600 text-xl"/>
-               تلفن:0911111111
-               </li>
-               <li className="flex items-center gap-3">
-                <FaLocationDot className="text-red-600 text-xl"/>
-              آدرس : تهران
-               </li>
-              </ul>
+              </li>
+              <li className="flex items-center gap-3">
+                <FaPhone className="text-red-600 text-xl" />
+                تلفن:0911111111
+              </li>
+              <li className="flex items-center gap-3">
+                <FaLocationDot className="text-red-600 text-xl" />
+                آدرس : تهران
+              </li>
+            </ul>
           </div>
         </div>
       </section>
     </div>
   );
-}
+};
 
 export default Contact;
