aed1e046d14e5831620d03f65d017450cd028895
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-20 15:01:23 +0330

    finish profile menu

--- part2/1-dropmenuprofile/src/components/MenuProfile/MenuProfile.tsx
+++ part2/1-dropmenuprofile/src/components/MenuProfile/MenuProfile.tsx
@@ -1,12 +1,30 @@
-import type { ReactNode } from "react"
+import { useState, type ReactNode } from "react"
 
 import cr7 from "../../assets/images/cr7.webp";
 
 type MenuProfileProps={};
 
  const MenuProfile = ({}:MenuProfileProps):ReactNode => {
+
+  const [isOpen,setIsOpen]=useState(false);
   return (
-    <div className="">Hello from MenuProfile</div>
+    <div className="flex items-center justify-center min-h-screen relative">
+      {isOpen&&<div onClick={()=>setIsOpen(false)} className="fixed inset-0 bg-black/30 z-10 cursor-pointer"></div>}
+      <div className="z-20" onClick={(e)=>setIsOpen(!isOpen)}>
+        <img className="w-20 h-20 object-cover  rounded-full border border-gray-600 hover:ring-1 hover:ring-blue-400 hover:ring-offset-1 cursor-pointer hover:shadow-lg transition-all duration-300" src={cr7} alt="cr7" />
+      </div>
+      <div className={`bg-white shadow-md w-full z-20 max-w-75 rounded-2xl overflow-hidden fixed left-[52%] top-[50%] transition-all duration-300 ${isOpen ? "ease-in translate-y-0":"opacity-0  translate-y-[-10%] pointer-events-none"} `}>
+        <div className="p-2 border-b border-b-gray-300 flex flex-col text-center gap-y-2 ">
+          <strong className="border-b border-b-gray-400">cr7</strong>
+          <small className="text-gray-300">cr7@gmail.com</small>
+        </div>
+        <ul className=" [&>li]:bg-blue-400 [&>li]:p-2 [&>li]:border-b [&>li]:border-b-gray-300 ">
+          <li>my profile</li>
+          <li>setting</li>
+          <li>log out</li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
