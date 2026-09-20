79e8c93562b3e682be46a98a3e19a13b66133f6b
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-20 12:12:50 +0330

    add new part for mini projects

---
+++ part2/1-dropmenuprofile/src/components/MenuProfile/MenuProfile.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react"
+
+import cr7 from "../../assets/images/cr7.webp";
+
+type MenuProfileProps={};
+
+ const MenuProfile = ({}:MenuProfileProps):ReactNode => {
+  return (
+    <div className="">Hello from MenuProfile</div>
+  );
+}
+
+export default MenuProfile;
---
+++ part2/1-dropmenuprofile/src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode } from "react"
+import MenuProfile from "../../components/MenuProfile/MenuProfile";
+
+type HomeProps={};
+
+ const Home = ({}:HomeProps):ReactNode => {
+  return (
+    <>
+    <MenuProfile/>
+    </>
+  );
+}
+
+export default Home;
