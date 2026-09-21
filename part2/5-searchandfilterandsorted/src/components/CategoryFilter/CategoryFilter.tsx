2e6186bbc376f8938aaf9d3faba7dba0e84f3035
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-21 21:27:31 +0330

    add static data

---
+++ part2/5-searchandfilterandsorted/src/components/CategoryFilter/CategoryFilter.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react"
+
+type CategoryFilterProps={};
+
+ const CategoryFilter = ({}:CategoryFilterProps):ReactNode => {
+  return (
+    <div className="">Hello from CategoryFilter</div>
+  );
+}
+
+export default CategoryFilter;
---
+++ part2/5-searchandfilterandsorted/src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode } from "react"
+import CategoryFilter from "../../components/CategoryFilter/CategoryFilter";
+
+type HomeProps={};
+
+ const Home = ({}:HomeProps):ReactNode => {
+  return (
+    <>
+    <CategoryFilter/>
+    </>
+  );
+}
+
+export default Home;
