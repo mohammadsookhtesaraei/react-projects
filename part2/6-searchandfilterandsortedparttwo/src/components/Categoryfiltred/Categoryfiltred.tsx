331c8fa4300ff122ec282420b70c421ef253e5ea
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-22 00:03:37 +0330

    start filter project2

---
+++ part2/6-searchandfilterandsortedparttwo/src/components/Categoryfiltred/Categoryfiltred.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react"
+
+type CategoryfiltredProps={};
+
+ const Categoryfiltred = ({}:CategoryfiltredProps):ReactNode => {
+  return (
+    <div className="">Hello from Categoryfiltred</div>
+  );
+}
+
+export default Categoryfiltred;
---
+++ part2/6-searchandfilterandsortedparttwo/src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react"
+import Categoryfiltred from "../../components/Categoryfiltred/Categoryfiltred";
+
+
+type HomeProps={};
+
+ const Home = ({}:HomeProps):ReactNode => {
+  return (
+    <div className="">
+     <Categoryfiltred/>
+    </div>
+  );
+}
+
+export default Home;
