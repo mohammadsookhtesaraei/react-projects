2a7187a7462288f8eb4e8f3f9f0310a317304d3b
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-21 14:21:11 +0330

    add totalprice and total qty in header

--- part2/4-shoppingcart/src/Components/Header/Header.tsx
+++ part2/4-shoppingcart/src/Components/Header/Header.tsx
@@ -1,17 +1,32 @@
-import { useState, type ReactNode } from "react";
+import { use, useState, type ReactNode } from "react";
 import { FaTimes } from "react-icons/fa";
 import { GiShoppingBag } from "react-icons/gi";
+import { CartContext } from "../../context/cart-context";
 
 type HeaderProps = {};
 
 const Header = ({}: HeaderProps): ReactNode => {
+
+  const { cart, disPatchItem } = use(CartContext);
+
+  // total price
+  const totalPrice = cart.reduce(
+    (total, item) =>
+      total + (item.qty ?? 0) * +item.price.split(" ")[0].split(",").join(""),
+    0,
+  );
+  // total qty
+  const totalqty = cart.reduce((total, item) => total + (item.qty ?? 0), 0);
+
+  // state cart basket
   const [isOpenCart, setIsOpenCart] = useState(false);
+
   return (
     <>
       <header className="border-b border-b-gray-300/30 h-15 shadow-md flex items-center  px-8  relative">
         <div className="flex relative">
           <span className="size-5 absolute   bottom-3 left-5 bg-rose-600 grid place-content-center rounded-full text-white text-sm">
-            0
+            {totalqty}
           </span>
           <GiShoppingBag
             size={24}
@@ -21,28 +36,60 @@ const Header = ({}: HeaderProps): ReactNode => {
           />
         </div>
       </header>
-
+      
+      {/* bacldrop */}
       {isOpenCart && (
         <div
           className="fixed z-10  inset-0 bg-black/30"
           onClick={() => setIsOpenCart(false)}
         ></div>
       )}
-
+      
+      {/* sidebar */}
       <div
         className={`fixed z-20 transition-transform duration-300 bg-white w-75 shadow-md ${isOpenCart ? "translate-x-0" : "translate-x-full"}`}
       >
         <div className="p-7 border-b border-b-gray-400/30 flex items-center justify-between">
           <h2 className="text-rose-500 text-lg font-semibold">سبد خرید</h2>
           <div>
-             <FaTimes
-                onClick={() => setIsOpenCart(false)}
-                className="text-rose-700 cursor-pointer"
-              />
+            <FaTimes
+              onClick={() => setIsOpenCart(false)}
+              className="text-rose-700 cursor-pointer"
+            />
           </div>
         </div>
-
-        
+        <div>
+          <div>
+            <div>
+              {!cart.length ? (
+                <p>سبد شما خالیست</p>
+              ) : (
+                <>
+                  {cart.map((item) => (
+                    <div>
+                      <img
+                        className="size-20 object-cover"
+                        src={item.image}
+                        alt={item.title}
+                      />
+                      <p>تعداد سفارش : {item.qty}</p>
+                    </div>
+                  ))}
+                </>
+              )}
+            </div>
+            <p className="text-rose-500 flex items-center">
+              قیمت نهایی :
+              <span className="text-gray-400">
+                {" "}
+                {totalPrice.toLocaleString()}تومان
+              </span>
+            </p>
+            <button onClick={() => disPatchItem({ type: "clear" })}>
+              حذف سبد خرید
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
