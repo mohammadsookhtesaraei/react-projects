a1eb4c589ac2dafa11f3f740cb9dfdb668b55dd7
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-21 11:37:24 +0330

    add tailwind

--- part2/4-shoppingcart/src/App.tsx
+++ part2/4-shoppingcart/src/App.tsx
@@ -1,6 +1,10 @@
+import Home from "./pages/Home/Home"
+
 const App = () => {
   return (
-    <>App</>
+    <>
+    <Home/>
+    </>
   )
 }
 export default App
---
+++ part2/4-shoppingcart/src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react"
+
+type HomeProps={};
+
+ const Home = ({}:HomeProps):ReactNode => {
+  return (
+    <div className="">Hello from Home</div>
+  );
+}
+
+export default Home;
