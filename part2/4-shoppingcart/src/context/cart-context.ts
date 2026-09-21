edfc39e7566e319442fb478fe0f7f250de41bb6b
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-21 12:41:52 +0330

    add reducer

---
+++ part2/4-shoppingcart/src/context/cart-context.ts
@@ -0,0 +1,7 @@
+import { createContext,type ActionDispatch } from "react";
+
+
+type ContextValue={}
+
+
+export const CartContext=createContext({} as ContextValue);
---
+++ part2/4-shoppingcart/src/reducers/cart-reducer.ts
@@ -0,0 +1,86 @@
+import { type IBook } from "../types/books-interface"
+
+
+type TState = IBook & {
+    qty: number
+};
+
+type State = {
+    cart: TState[]
+};
+
+
+
+type CartAction = { type: "Add_TO_Cart", payload: TState } | { type: "Increase", payload: TState } | { type: "Decrease", payload: TState } | { type: "Remove_Item", payload: TState } | { type: "clear" };
+
+
+
+export const reducerCart = (state: State, action: CartAction) => {
+
+    switch (action.type) {
+        case "Add_TO_Cart": {
+            const isExist = state.cart.find((item) => item.id === action.payload.id);
+            if (!isExist) {
+                return { ...state, cart: [...state.cart,{ ...action.payload, qty: 1 }] }
+            } else {
+                return state
+            }
+        }
+
+        case "Increase": {
+            const isExist = state.cart.find((item) => item.id === action.payload.id);
+            if (isExist) {
+                return {
+                    ...state,
+                    cart: state.cart.map((item) => {
+                        if (item.id === action.payload.id) {
+                            return { ...item, qty: (item.qty ?? 0 ) + 1}
+                        } else {
+                            return item
+                        }
+                    })
+                }
+            } else {
+                return state
+            }
+        }
+
+        case "Decrease": {
+            const isExist = state.cart.find((item) => item.id === action.payload.id);
+            if (isExist) {
+                return {
+                    ...state,
+                    cart: state.cart.map((item) => {
+                        if (item.id === action.payload.id) {
+                            return { ...item, qty: (item.qty ?? 0 ) + 1}
+                        } else {
+                            return item
+                        }
+                    })
+                }
+            } else {
+                return state
+            }
+        }
+
+        case "Remove_Item": {
+            return {
+                ...state,
+                cart: state.cart.filter((item) => item.id !== action.payload.id)
+            }
+        }
+
+        case "clear": {
+            return {
+                ...state,
+                cart: []
+            }
+        }
+
+        default: return state
+
+
+
+    }
+
+}
